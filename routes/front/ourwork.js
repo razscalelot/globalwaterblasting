2c781be5427c1ae2d6761e74e79a0a3de2251388
razscalelot <you@example.com>
2023-10-24 14:36:55 +0530

    mobile number validation

--- routes/front/ourwork.js
+++ routes/front/ourwork.js
@@ -36,53 +36,59 @@ router.get('/', function (req, res, next) {
 
 router.post('/', async (req, res) => {
   let { name, email, mobile, address, postcode, message } = req.body;
-  if (name && name.trim() != '' && email && email.trim() != '' && mobile && mobile != '' && address && address.trim() != '' && postcode && postcode != '') {
-    let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-    let offerData = await primary.model(constants.MODELS.offers, offerModel).findOne({ $or: [{ mobile: mobile }, { email: email }] }).lean();
-    if (offerData == null) {
-      let obj = {
-        name: name,
-        email: email,
-        mobile: mobile,
-        address: address,
-        postcode: postcode,
-        message: message
-      }
-      let insertedData = await primary.model(constants.MODELS.offers, offerModel).create(obj)
-      if (insertedData && insertedData != null) {
-        const tranEmailApi = new Sib.TransactionalEmailsApi()
-        const sender = {
+  if (name && name.trim() != '' && mobile && mobile != '') {
+    if ((/^(\+?\d{1,3})?([0-9]{3})([0-9]{3})([0-9]{4})$/.test(mobile))) {
+      let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+      let offerData = await primary.model(constants.MODELS.offers, offerModel).findOne({ $or: [{ mobile: mobile }, { email: email }] }).lean();
+      if (offerData == null) {
+        let obj = {
+          name: name,
           email: email,
-          name: name
+          mobile: mobile,
+          address: address,
+          postcode: postcode,
+          message: message
         }
-        const receivers = [
-          {
-            email: process.env.SIB_EMAIL_ID,
-            name: 'Global Water Blasting'
-          },
-        ];
-        tranEmailApi.sendTransacEmail({
-          sender,
-          to: receivers,
-          subject: 'Special Offer',
-          htmlContent: `<h2>Customer Information</h2><h3> Customer Email: ` + email + `</h3><h3>Customer Mobile No.: ` + mobile + `</h3><h3>Customer Address: ` + address + ` ` + postcode + `</h3><h3>Customer Message: ` + message + `</h3>`,
-        }).then((response) => {
-          req.flash('message', 'Your message has been successfully sent. We will contact you very soon!');
+        let insertedData = await primary.model(constants.MODELS.offers, offerModel).create(obj)
+        if (insertedData && insertedData != null) {
+          const tranEmailApi = new Sib.TransactionalEmailsApi()
+          email = (email) ? email : 'example@gmail.com'
+          const sender = {
+            email: email,
+            name: name
+          }
+          const receivers = [
+            {
+              email: process.env.SIB_EMAIL_ID,
+              name: 'Global Water Blasting'
+            },
+          ];
+          tranEmailApi.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: 'Special Offer',
+            htmlContent: `<h2>Customer Information</h2><h3> Customer Email: ` + email + `</h3><h3>Customer Mobile No.: ` + mobile + `</h3><h3>Customer Address: ` + address + ` ` + postcode + `</h3><h3>Customer Message: ` + message + `</h3>`,
+          }).then((response) => {
+            req.flash('message', 'Your message has been successfully sent. We will contact you very soon!');
+            res.redirect('/ourwork');
+          }).catch((error) => {
+            req.flash('message', error);
+            res.redirect('/ourwork');
+          });
+        } else {
+          req.flash('message', 'Something went wrong, Please try again');
           res.redirect('/ourwork');
-        }).catch((error) => {
-          req.flash('message', error);
-          res.redirect('/ourwork');
-        });
+        }
       } else {
-        req.flash('message', 'Something went wrong, Please try again');
+        req.flash('message', 'The request message was already sent. We will contact you very soon!');
         res.redirect('/ourwork');
       }
     } else {
-      req.flash('message', 'The request message was already sent. We will contact you very soon!');
-      res.redirect('/ourwork');
+      req.flash('message', 'Invalid phone number, please try again');
+      res.redirect('/contactus');
     }
   } else {
-    req.flash('message', 'Invalid name, email, mobile, address, postcode and message can not be empty, please try again');
+    req.flash('message', 'Invalid name and phone number can not be empty, please try again');
     res.redirect('/ourwork');
   }
 });
--- routes/front/servicedetails.js
+++ routes/front/servicedetails.js
@@ -45,53 +45,59 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   let { name, email, mobile, address, selected_service, massage, redirect } = req.body;
-  if (name && name.trim() != '' && email && email.trim() != '' && mobile && mobile != '' && address && address.trim() != '' && selected_service && selected_service != '') {
-    let primary = mongoConnection.useDb(constants.DEFAULT_DB);
-    let contactusData = await primary.model(constants.MODELS.quotes, quoteModel).findOne({ $or: [{ mobile: mobile }, { email: email }] }).lean();
-    if (contactusData == null) {
-      let obj = {
-        name: name,
-        email: email,
-        mobile: mobile,
-        address: address,
-        selected_service: selected_service,
-        massage: massage
-      }
-      let insertedData = await primary.model(constants.MODELS.quotes, quoteModel).create(obj)
-      if (insertedData && insertedData != null) {
-        const tranEmailApi = new Sib.TransactionalEmailsApi()
-        const sender = {
+  if (name && name.trim() != '' && mobile && mobile != '' && selected_service && selected_service != '') {
+    if ((/^(\+?\d{1,3})?([0-9]{3})([0-9]{3})([0-9]{4})$/.test(mobile))) {
+      let primary = mongoConnection.useDb(constants.DEFAULT_DB);
+      let contactusData = await primary.model(constants.MODELS.quotes, quoteModel).findOne({ $or: [{ mobile: mobile }, { email: email }] }).lean();
+      if (contactusData == null) {
+        let obj = {
+          name: name,
           email: email,
-          name: name
+          mobile: mobile,
+          address: address,
+          selected_service: selected_service,
+          massage: massage
         }
-        const receivers = [
-          {
-            email: process.env.SIB_EMAIL_ID,
-            name: 'Global Water Blasting'
-          },
-        ];
-        tranEmailApi.sendTransacEmail({
-          sender,
-          to: receivers,
-          subject: 'Service Enquiry: ' + selected_service,
-          htmlContent: `<h2>Customer Information</h2><h3> Customer Email: ` + email + `</h3><h3>Customer Mobile No.: ` + mobile + `</h3><h3>Customer Address: ` + address + `</h3><h3>Interested Service: ` + selected_service + `</h3><h3>Customer Message: ` + massage + `</h3>`,
-        }).then((response) => {
-          req.flash('message', 'Your message has been successfully sent. We will contact you very soon!');
+        let insertedData = await primary.model(constants.MODELS.quotes, quoteModel).create(obj)
+        if (insertedData && insertedData != null) {
+          const tranEmailApi = new Sib.TransactionalEmailsApi()
+          email = (email) ? email : 'example@gmail.com'
+          const sender = {
+            email: email,
+            name: name
+          }
+          const receivers = [
+            {
+              email: process.env.SIB_EMAIL_ID,
+              name: 'Global Water Blasting'
+            },
+          ];
+          tranEmailApi.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: 'Service Enquiry: ' + selected_service,
+            htmlContent: `<h2>Customer Information</h2><h3> Customer Email: ` + email + `</h3><h3>Customer Mobile No.: ` + mobile + `</h3><h3>Customer Address: ` + address + `</h3><h3>Interested Service: ` + selected_service + `</h3><h3>Customer Message: ` + massage + `</h3>`,
+          }).then((response) => {
+            req.flash('message', 'Your message has been successfully sent. We will contact you very soon!');
+            res.redirect('/servicedetails?name=' + redirect);
+          }).catch((error) => {
+            req.flash('message', error);
+            res.redirect('/servicedetails?name=' + redirect);
+          });
+        } else {
+          req.flash('message', 'Something went wrong, Please try again');
           res.redirect('/servicedetails?name=' + redirect);
-        }).catch((error) => {
-          req.flash('message', error);
-          res.redirect('/servicedetails?name=' + redirect);
-        });
+        }
       } else {
-        req.flash('message', 'Something went wrong, Please try again');
+        req.flash('message', 'The request message was already sent. We will contact you very soon!');
         res.redirect('/servicedetails?name=' + redirect);
       }
     } else {
-      req.flash('message', 'The request message was already sent. We will contact you very soon!');
-      res.redirect('/servicedetails?name=' + redirect);
+      req.flash('message', 'Invalid phone number, please try again');
+      res.redirect('/contactus');
     }
   } else {
-    req.flash('message', 'Invalid name, email, mobile, address, select service and message can not be empty, please try again');
+    req.flash('message', 'Invalid name and phone number can not be empty, please try again');
     res.redirect('/servicedetails?name=' + redirect);
   }
 });
