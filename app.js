8eca58826c56d63c4765784bdb491044cbc755f4
dprashant <dprashant.scalelot@gmail.com>
2024-06-19 09:59:41 +0530

    cntact us

--- app.js
+++ app.js
@@ -85,9 +85,9 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.status(err.status || 500);
   res.render('error', { layout: false });
 });
 module.exports = app;
